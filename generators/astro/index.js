bb27913818a014d8c52a31acc08b09b4555b685f
Nico Ismaili <nico.ismaili@aoe.com>
2024-06-14 14:24:00 +0200

    feat: made additional features selectable
    /spend 10m

--- generators/astro/index.js
+++ generators/astro/index.js
@@ -7,6 +7,7 @@ const AdditionalFeatures = {
 	E2E_TESTS: `Playwright`,
 	GQL: `GraphQL TS SDK generation`,
 	DOT_ENV: `dotenvx`,
+	FAVICON_GEN: `Favicon generation`,
 };
 
 module.exports = class extends Generator {
@@ -48,34 +49,41 @@ module.exports = class extends Generator {
 					default: `ssg-project`,
 				},
 			]));
+
 		this.projectDescription =
 			this.options[PromptItems.PROJECT_DESCRIPTION] ||
-			(await this.prompt([
+			(
+				await this.prompt([
+					{
+						type: `input`,
+						name: PromptItems.PROJECT_DESCRIPTION,
+						message: `Write a brief description of your app`,
+						default: `A static site generator project`,
+					},
+				])
+			)[PromptItems.PROJECT_DESCRIPTION];
+
+		this.appTitle = (
+			await this.prompt([
 				{
 					type: `input`,
-					name: PromptItems.PROJECT_DESCRIPTION,
-					message: `Write a brief description of your app`,
-					default: `A static site generator project`,
+					name: `site`,
+					message: `What is the title of your website?`,
+					default: `Astro App`,
 				},
-			]));
+			])
+		).site;
 
-		this.appTitle = await this.prompt([
-			{
-				type: `input`,
-				name: `site`,
-				message: `What is the title of your website?`,
-				default: `Astro App`,
-			},
-		]);
-
-		this.additionalFeatures = await this.prompt([
-			{
-				type: `checkbox`,
-				name: `features`,
-				message: `Select additional features to include in your project`,
-				choices: Object.values(AdditionalFeatures),
-			},
-		]);
+		this.additionalFeatures = (
+			await this.prompt([
+				{
+					type: `checkbox`,
+					name: `features`,
+					message: `Select additional features to include in your project`,
+					choices: Object.values(AdditionalFeatures),
+				},
+			])
+		).features;
 	}
 
 	writing() {
@@ -115,6 +123,26 @@ module.exports = class extends Generator {
 		this.composeWith(require.resolve(`../husky`));
 		this.composeWith(require.resolve(`../lint`));
 		this.composeWith(require.resolve(`../renovate`));
+
+		this.additionalFeatures.forEach((feature) => {
+			switch (feature) {
+				case AdditionalFeatures.I18N:
+					this.composeWith(require.resolve(`../astro-i18n`));
+					break;
+				case AdditionalFeatures.E2E_TESTS:
+					this.composeWith(require.resolve(`../playwright`));
+					break;
+				case AdditionalFeatures.GQL:
+					this.composeWith(require.resolve(`../gql`));
+					break;
+				case AdditionalFeatures.DOT_ENV:
+					this.composeWith(require.resolve(`../env`));
+					break;
+				case AdditionalFeatures.FAVICON_GEN:
+					this.composeWith(require.resolve(`../favicon`));
+					break;
+			}
+		});
 	}
 
 	install() {
