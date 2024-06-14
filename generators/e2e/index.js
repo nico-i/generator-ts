073012149bcfc03ec3ee0dfd0dee92718c429080
Nico Ismaili <nico.ismaili@aoe.com>
2024-06-14 11:35:22 +0200

    feat: added playwright e2e generator
    /spend 5m

--- generators/e2e/index.js
+++ generators/e2e/index.js
@@ -1,4 +1,12 @@
 "use-strict";
 const Generator = require(`yeoman-generator`);
 
-module.exports = class extends Generator {};
+module.exports = class extends Generator {
+	constructor(args, opts) {
+		super(args, opts);
+	}
+
+	installPlaywright() {
+		this.spawnCommand(`bun`, [`create`, `playwright@latest`]);
+	}
+};
