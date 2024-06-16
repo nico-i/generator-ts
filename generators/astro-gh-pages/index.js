318c5fd43de1e7813e7106628e9c5e72a888c42a
Nico Ismaili <nico.ismaili@aoe.com>
2024-06-16 11:22:32 +0200

    feat: added release-please setup option to gh-pages generator + added tsconfig
    /spend 10m

--- generators/astro-gh-pages/index.js
+++ generators/astro-gh-pages/index.js
@@ -3,7 +3,24 @@ const Generator = require(`yeoman-generator`);
 const { Format } = require(`../../lib/Format`);
 
 module.exports = class extends Generator {
+	prompting() {
+		const answers = this.prompt([
+			{
+				type: `confirm`,
+				name: `setupReleasePlease`,
+				message: `Do you to add a "release-please" action to automate the creation of releases?`,
+				default: true,
+			},
+		]);
+
+		this.setupReleasePlease = answers.setupReleasePlease;
+	}
+
 	writing() {
+		if (this.setupReleasePlease) {
+			this.composeWith(require.resolve(`../release-please`));
+		}
+
 		this.log(Format.step(`Adding GitHub actions workflow`));
 		this.fs.copy(this.templatePath(), this.destinationPath());
 		this.log(Format.success(`Action added successfully`));
