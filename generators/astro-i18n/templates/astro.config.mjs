2549fdf1fcae50e3e1db54e38746811ba45a644d
Nico Ismaili <nico.ismaili@aoe.com>
2024-07-18 16:12:27 +0200

    chore: removed transpiled code
    /spend 5m

--- generators/astro-i18n/templates/astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import astroI18next from "astro-i18next";
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-	integrations: [astroI18next()],
-});
--- generators/husky/templates/commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	extends: ["@commitlint/config-conventional"],
-	plugins: ["commitlint-plugin-spend"],
-	rules: {
-		spend: [2, "always"],
-	},
-};
